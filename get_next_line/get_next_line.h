af349a02d20aa0974e66418d66f8a1bd5e88b5fa
Fabio Andre Silva Pinto Bastos <fsilva-p@c2r7s1.42luxembourg.lu>
2024-04-24 18:58:53 +0200

    added

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,8 +6,26 @@
 /*   By: fsilva-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 16:49:31 by fsilva-p          #+#    #+#             */
-/*   Updated: 2024/04/24 16:49:32 by fsilva-p         ###   ########.fr       */
+/*   Updated: 2024/04/24 18:58:40 by fsilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
 
+typedef struct s_list
+{
+	char	letter;
+	struct s_list *next;
+}	t_list;
+
+char *get_next_line(int fd);
+
+#endif
