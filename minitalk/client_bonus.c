d4eac625b68b3d0e64f722d9657cd7fb37cf9489
fabio <155982713+fabiosilva24@users.noreply.github.com>
2024-06-22 13:01:20 +0200

    Update client_bonus.c

--- minitalk/client_bonus.c
+++ minitalk/client_bonus.c
@@ -1,5 +1,22 @@
 #include <unistd.h>
 #include "minitalk_bonus.h"
+void	handle_signal(int signal)
+{
+	static int	bits = 0;
+	static char	c = 0;
+
+	if (signal == SIGUSR1)
+		c |= (0 << bits);
+	else if (signal == SIGUSR2)
+		c |= (1 << bits);
+	bits++;
+	if (bits == 8)
+	{
+		write(1, &c, 1);
+		bits = 0;
+		c = 0;
+	}
+}
 
 void	send_char(pid_t pid, char c)
 {
@@ -22,10 +39,18 @@ int	main(int argc, char **argv)
 	pid_t	server_pid;
 	char	*message;
 	int		i;
+	struct sigaction sa;
+	
+	sa.sa_handler = handle_signal;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
 
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
 	i = 0;
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
 	if (argc != 3)
 	{
 		ft_printf("Use: %s <server_pid> <message>", argv[0]);
