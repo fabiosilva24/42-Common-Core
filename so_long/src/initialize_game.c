dbc5325ff8f3b2568028e176c1eefd0c4844423a
fsilva-p <fsilva-p@student.42luxembourg.lu>
2024-10-04 14:16:14 +0200

    added

--- so_long/src/initialize_game.c
+++ so_long/src/initialize_game.c
@@ -2,9 +2,17 @@ void initialize_game(t_game *game)
 {
 	game->mlx_ptr = mlx_init();
 	if (!game->mlx_ptr)
-		exit (1);
+	{
+		write(2, "Error initializing MLX\n", 23);
+		exit(EXIT_FAILURE);
+	}
 	game->win_ptr = mlx_new_window(game->mlx_ptr, 800, 600, "So Long");
 	if (!game->win_ptr)
-		exit(1);
+	{
+		write(2, "Error creating the window\n", 23);
+		exit(EXIT_FAILURE);
+	}
+
+	game->background_img = mlx_xpm_file_to_image(game->mlx_ptr, ""
 
 }
