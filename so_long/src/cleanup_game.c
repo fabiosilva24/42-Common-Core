9fb056a99479585f4ac3fbdc4e42e314471a547c
fabio <155982713+fabiosilva24@users.noreply.github.com>
2024-10-07 17:11:20 +0200

    Update cleanup_game.c

--- so_long/src/cleanup_game.c
+++ so_long/src/cleanup_game.c
@@ -4,8 +4,8 @@ void cleanup_game(t_game *game)
 {
 	if (game->win_ptr)
 		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
-	if (game->background_img)
-		mlx_destroy_window(game->mlx_ptr, game->background_img);
+	if (game->background_image)
+		mlx_destroy_window(game->mlx_ptr, game->background_image);
 	if (game->mlx_ptr)
 		free(game->mlx_ptr);
 }
