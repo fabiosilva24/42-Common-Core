24dbad985034adeebb88f5de2cedeabd52f390ab
Fabio Andre Silva Pinto Bastos <fsilva-p@c2r9s1.42luxembourg.lu>
2024-10-14 19:20:55 +0200

    added

--- so_long/src/flood_fill.c
+++ so_long/src/flood_fill.c
@@ -3,39 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   flood_fill.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsilva-p <fsilva-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fsilva-p <fsilva-p@42luxembourg.lu>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:00:07 by fsilva-p          #+#    #+#             */
-/*   Updated: 2024/10/13 16:02:39 by fsilva-p         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:17:33 by fsilva-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void	flood_fill(char **map, t_player size, t_player cur, char to_fill)
+void	flood_fill(t_game *game, t_player size, t_player cur, char to_fill)
 {
 	if (cur.x < 0 || cur.x >= size.x || cur.y < 0 || cur.y >= size.y
-		|| map[cur.y][cur.x] != to_fill)
+		|| game->map[cur.y][cur.x] != to_fill)
 		return ;
-	map[cur.y][cur.x] = 'P';
+	game->map[cur.y][cur.x] = 'P';
 	flood_fill(map, size, (t_player){cur.x - 1, cur.y}, to_fill);
 	flood_fill(map, size, (t_player){cur.x + 1, cur.y}, to_fill);
 	flood_fill(map, size, (t_player){cur.x, cur.y - 1}, to_fill);
 	flood_fill(map, size, (t_player){cur.x, cur.y + 1}, to_fill);
 }
 
-int	is_valid_move(char **map, t_player size, t_player current, t_player next)
+int	is_valid_move(t_game *game, t_player size, t_player current, t_player next)
 {
 	char	next_position;
 
 	if (next.x < 0 || next.x >= size.x || next.y < 0 || next.y >= size.y)
 		return (0);
-	next_position = map[next.y][next.x];
+	next_position = game->map[next.y][next.x];
 	return (next_position == '0' || next_position == 'C'
 		|| next_position == 'E');
 }
 
-t_player	move_player(char **map, t_player size, t_player current)
+t_player	move_player(t_game *game, t_player size, t_player current)
 {
 	t_player	next;
 	int move_x;
@@ -43,10 +43,10 @@ t_player	move_player(char **map, t_player size, t_player current)
 
 	next.x = current.x + move_x;
 	next.y = current.y + move_y;
-	if (is_valid_move(map, size, current, next))
+	if (is_valid_move(game->map, size, current, next))
 	{
-		map[current.y][current.x] = '0';
-		map[next.y][next.x] = 'P';
+		game->map[current.y][current.x] = '0';
+		game->map[next.y][next.x] = 'P';
 		return (next);
 	}
 	return (current);
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -1,8 +1,26 @@
 #include "../include/so_long.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+	t_game game;
+
 	if (argc != 2)
-		write(1, "\n", 1);
-	
+	{
+		ft_printf("Usage : ./so_long map\n");
+		return (0);
+	}
+
+	initialize_game(&game);
+	if (!map_draw(&game, argv[1]) || !is_rectangle(&game) || !is_closed_by_walls(&game))
+	{
+		cleanup_game(&game);
+		ft_printf("Invalid map\n");
+	}
+
+	handle_input(&game);
+
+	mlx_loop(game.mlx_ptr);
+
+	cleanup_game(&game);
+	return (0);
 }
