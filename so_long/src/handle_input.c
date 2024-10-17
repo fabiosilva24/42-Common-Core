da5ac9f687dd50f490661255553b4e6d19820d4b
fabio <155982713+fabiosilva24@users.noreply.github.com>
2024-10-17 23:00:51 +0200

    Atualizar o handle_input.c

--- so_long/src/handle_input.c
+++ so_long/src/handle_input.c
@@ -2,6 +2,6 @@
 
 void handle_input(t_game *game)
 {
-	  mlx_hook(game->win_ptr, 2, 1L<<0,          handle_keypress, game);  // 2 = key press event
+	  mlx_hook(game->win_ptr, 2, 1L<<0,handle_keypress, game);  // 2 = key press event
 
 }
