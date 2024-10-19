d6a1141410f7b50c42a00f4bcdd5cfb5cf7ef83d
fabio silva <fabiosilva@fedora.fritz.box>
2024-10-20 01:03:50 +0200

    fixed norminette error

--- so_long/src/handle_input.c
+++ so_long/src/handle_input.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   handle_input.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsilva-p <fsilva-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fabiosilva <fabiosilva@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 23:49:58 by fsilva-p          #+#    #+#             */
-/*   Updated: 2024/10/18 23:50:02 by fsilva-p         ###   ########.fr       */
+/*   Updated: 2024/10/20 01:03:30 by fabiosilva       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int handle_window_close(t_game *game)
+int	handle_window_close(t_game *game)
 {
 	ft_printf("Window close button clicked, exiting game\n");
 	game->should_end = 1;
@@ -20,8 +20,8 @@ int handle_window_close(t_game *game)
 	return (0);
 }
 
-void handle_input(t_game *game)
+void	handle_input(t_game *game)
 {
-	mlx_hook(game->win_ptr, 2, 1L<<0, handle_keypress, game);
-	mlx_hook(game->win_ptr, 17, 1L<<17, handle_window_close, game);
+	mlx_hook(game->win_ptr, 2, 1L << 0, handle_keypress, game);
+	mlx_hook(game->win_ptr, 17, 1L << 17, handle_window_close, game);
 }
