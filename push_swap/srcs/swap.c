fb8b24d8457b2978cfe94b71bc946b0e8f0d8f76
fabocas <fabio_bastos03@hotmail.com>
2024-10-29 02:05:11 +0100

    added

---
+++ push_swap/srcs/swap.c
@@ -0,0 +1,33 @@
+#include "./include/push_swap.h"
+
+void swap(t_struct_nodes **head)
+{
+    if (!*head || !(*head)->next)
+        return ;
+    *head = (*head)->next;
+    (*head)->prev->prev = *head;
+    (*head)->prev->next = (*head)->next;
+    if ((*head)->next)
+        (*head)->next->prev;
+    (*head)->next = (*head)prev;
+    (*head)->prev = NULL;
+}
+
+void sa(t_struct_nodes **a)
+{
+    swap(a);
+    ft_printf("sa\n");
+}
+
+void sb(t_struct_node **b)
+{
+    swap(b);
+    ft_printf("sb\n");
+}
+
+void ss(t_struct_node **a, t_struct_node **b)
+{
+    swap(a);
+    swap(b);
+    ft_printf("ss\n");
+}
