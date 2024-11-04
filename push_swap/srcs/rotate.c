5d92d130b08165f6fe98a487228d393552237b94
fabocas <fabio_bastos03@hotmail.com>
2024-11-04 16:17:24 +0100

    added rotate

--- push_swap/srcs/rotate.c
+++ push_swap/srcs/rotate.c
@@ -2,12 +2,27 @@
 
 void rotate(t_struct_nodes **stack) //first becomes last       
 {
-    t_struct_nodes **last;
+    t_struct_nodes *last;
+    t_struct_node *newest_last;
 
     if (!*stack || !(*stack)->next)
         return ;
     last = find_lastnode(*stack);
+    newest_last = *stack;
+    while (newest_last->next != last)    //while loop until newest_last is the new last node
+    {
+        newest_last = newest_last->next;
+    }
+
+    newest_last->next = NULL;  //detach last node from the list by making newset_last the newest last node
+
+    last->next = *stack;
+    last->prev = NULL;
+
+    if (*stack)
+        (*stack)->prev = last_>node;  // Set the previous pointer of the current top to the new top
     
+    *stack = last;
 
 }
 
@@ -30,3 +45,4 @@ void rr(t_struct_nodes **a, t_struct_nodes **b)
     rotate(a);
     rotate(b);
     ft_printf("rr\n");
+}
