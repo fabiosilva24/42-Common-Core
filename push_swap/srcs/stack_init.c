d0eba56b9fbc08b6cd9002ea5ab2e9aa251184ba
fabocas <fabio_bastos03@hotmail.com>
2024-11-07 15:11:00 +0100

    append_node, get_cheapest

--- push_swap/srcs/stack_init.c
+++ push_swap/srcs/stack_init.c
@@ -22,6 +22,60 @@ static long ft_atol(const char *str)   //ascii to long
     return (sign * result);
 }
 
+static void	append_node(t_stack_nodes **stack, int n)
+{
+    t_stack_node *last_nodes;
+    t_stack_node *node;
+
+    if (!*stack)
+        return ;
+    node = malloc(sizeof(t_stack_nodes));
+    if (!node)
+        return ;
+    node->next = NULL;
+    node->nbr = n;
+    node->cheapest = 0;
+    if (!(*stack)) // If the stack is empty, initialize the first node
+    {
+        *stack = node;
+        node->prev = NULL;
+    }
+    else
+    {
+        last_node = find_lastnode;
+        last_node->next = node;     //seting last_node to next so now the next is the last
+        node->prev = last_node;    //nodes->prev is last node since node = next node node->prev last
+    }
+}
+void init_stack_a(t_stack nodes **stack)
+{
+    if (!*stack)
+        return ;
+    
+}
+
+
+t_stack_nodes *get_cheapest(t_stack nodes **stack)
+{
+    if  (*stack)
+        return ;
+    
+    while (stack)
+    {
+        if (stack->cheapest)
+                return stack;
+        stack = stack->next;
+    }
+    return NULL;
+}
+
+
+
+
+        
+
+
+
 /*int main(void)
 {
     const char *str = "1234567891111111111111111";
