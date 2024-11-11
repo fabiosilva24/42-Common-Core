bf0bbfa5fc8684e2c884e742d40ae1f15eea250d
fabocas <fsilva-p@student.42luxembourg.lu>
2024-11-11 17:13:47 +0100

    added

--- push_swap/srcs/push.c
+++ push_swap/srcs/push.c
@@ -1,14 +1,18 @@
-void push(t_stack_nodes **src, t_stack_nodes **dest)
+void push(t_stack_nodes **from_stack, t_stack_nodes **to_stack)
 {
-    if (stack)
+    if (!from_stack || !*from_stack)
         return ;
 
-
+    temp = *from_stack;
+    *from_stack = from_stack->next;
+    if (from_stack)
+        from_stack->prev = NULL;
+    temp->next = *to_stack;
+    if (*to_stack)
+        (*to_stack)->prev = NULL;
+    *to_stack = temp;
+    (*to_stack)->prev = NULL;
 }
-
-
-
-
 
 void pa(t_stack_nodes **a, t_stack_nodes **b)
 {
