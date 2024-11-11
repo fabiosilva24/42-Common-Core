07f9e365053291880dff6aefea8a506c1c02c491
fabocas <fsilva-p@student.42luxembourg.lu>
2024-11-11 16:07:59 +0100

    added

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -30,22 +30,22 @@ static long ft_atol(const char *str); //int to long
 //nodes init
 
 //stack utils
-t_stack_nodes *find_lastnode(t_struct_nodes *stack); //searches a stack and return the last node
-t_stack_nodes *find_maxnode(t_struct_nodes *stack); //searches a stack and returns the node with the biggest number    
+t_stack_nodes *find_lastnode(t_stack_nodes *stack); //searches a stack and return the last node
+t_stack_nodes *find_maxnode(t_stack_nodes *stack); //searches a stack and returns the node with the biggest number    
 //commands
-static void rotate(t_struct_nodes **stack); //first to last node
-static void reverse(t_struct_node **stack); //last to first node
-void			sa(t_stack_node **a);
-void			sb(t_stack_node **b);
-void			ss(t_stack_node **a, t_stack_node **b);
-void			ra(t_stack_node **a);
-void			rb(t_stack_node **b);
-void			rr(t_stack_node **a, t_stack_node **b);
-void			rra(t_stack_node **a);
-void			rrb(t_stack_node **b);
-void			rrr(t_stack_node **a, t_stack_node **b);
-void			pa(t_stack_node **a, t_stack_node **b);
-void			pb(t_stack_node **b, t_stack_node **a);
+static void rotate(t_stack_nodes **stack); //first to last node
+static void reverse(t_stack_nodes **stack); //last to first node
+void			sa(t_stack_nodes **a);
+void			sb(t_stack_nodes **b);
+void			ss(t_stack_nodes **a, t_stack_nodes **b);
+void			ra(t_stack_nodes **a);
+void			rb(t_stack_nodes **b);
+void			rr(t_stack_nodes **a, t_stack_nodes **b);
+void			rra(t_stack_nodes **a);
+void			rrb(t_stack_nodes **b);
+void			rrr(t_stack_nodes **a, t_stack_nodes **b);
+void			pa(t_stack_nodes **a, t_stack_nodes **b);
+void			pb(t_stack_nodes **b, t_stack_nodes **a);
 
 //algorithms
 void sort_three(t_stack_nodes **a);
