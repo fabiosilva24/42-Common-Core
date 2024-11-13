c7856d47e0bcf2ce9b5c9ebc95d8165196265acc
fabio <155982713+fabiosilva24@users.noreply.github.com>
2024-11-13 14:47:43 +0100

    Atualizar o init_a_to_b.c

--- push_swap/srcs/init_a_to_b.c
+++ push_swap/srcs/init_a_to_b.c
@@ -2,8 +2,24 @@
 
 void set_target(t_stack_nodes *a, t_stack_nodes *b)
 {
+    t_stack_nodes *current_b;
+    t_stack_nodes *target_node;
+
     if (!a || !b)
       return ;
+ b = current_b;
+
+  while (a)
+{
+    if (a->push_cost < a->cheapest_node)
+    {
+       a->push_cost = a->cheapest_node;
+       a = a->next;
+     }
+     cheapest_value = true;
+     
+    
+   
 
 }
 
