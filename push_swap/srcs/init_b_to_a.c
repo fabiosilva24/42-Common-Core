ee1d0e665a1b61f6afbb5c86ee9a1f6e5e27abef
fabocas <fabio_bastos03@hotmail.com>
2024-11-18 18:32:58 +0100

    fixed

--- push_swap/srcs/init_b_to_a.c
+++ push_swap/srcs/init_b_to_a.c
@@ -19,22 +19,22 @@ static void set_target_b(t_stack_nodes *a, t_stack_nodes *b)
     long closest_bigger_nbr;
     
  
-  while (a)
-{
-    closest_bigger_nbr = LONG_MAX;
-    current_a = a;
-    if (b->nbr < current_a->nbr
-        && current_a->nbr < closest_bigger_nbr)
+    while (a)
     {
-       closest_bigger_nbr = current_a->nbr;
-       target_node = current_a;
+        closest_bigger_nbr = LONG_MAX;
+        current_a = a;
+        if (current_a->nbr > b->nbr
+            && current_a->nbr < closest_bigger_nbr)
+        {
+        closest_bigger_nbr = current_a->nbr;
+        target_node = current_a;
+        }
+        current_a = current_a->next;
     }
-     current_a = current_a->next;
-}
-if (closest_bigger_nbr == LONG_MAX)
-    a->target_node = find_minnode(b);
-else
-    b->target_node = target_node;
+    if (closest_bigger_nbr == LONG_MAX)
+            b->target_node = find_minnode(b);
+    else
+            b->target_node = target_node;
     b = b->next;
 }
 
