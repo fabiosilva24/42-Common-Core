b728182b382463033f3fa67fa2a1353b34a655b0
fabocas <fabio_bastos03@hotmail.com>
2024-11-18 19:40:36 +0100

    fixed

--- push_swap/srcs/sort_stacks.c
+++ push_swap/srcs/sort_stacks.c
@@ -34,7 +34,7 @@ static void move_a_to_b(t_stack_nodes **a, t_stack_nodes **b)
 {
     t_stack_nodes *cheapest_node;
 
-    cheapest_node(*a) = get_cheapest(*a);
+    cheapest_node = get_cheapest(*a);
     if (cheapest_node->above_median && cheapest_node->target_node->above_median)
         rotate_both(a, b, cheapest_node);
     else if (!(cheapest_node->above_median)
