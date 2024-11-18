11bc7c267718ee25cf0186fd96367bd9512fb022
fabocas <fabio_bastos03@hotmail.com>
2024-11-18 19:56:00 +0100

    fixed

--- push_swap/srcs/swap.c
+++ push_swap/srcs/swap.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./include/push_swap.h"
+#include "../include/push_swap.h"
 
 void swap(t_stack_nodes **head)
 {
