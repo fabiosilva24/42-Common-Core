3f9ac896b16df0e7f2fc9ea06403a70b6fab2451
fabocas <fabio_bastos03@hotmail.com>
2024-11-18 20:14:05 +0100

    added

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -21,7 +21,10 @@ int main(int argc, char **argv)
     b = NULL;
     
 	if (argc == 1 && (argc == 2 && !argv[1][0]))
+    {
+        ft_printf("Error: No input provided\n");
         return (1);
+    }
     else if (argc == 2)
     argv = modified_ftsplit(argv[1], ' ');
     init_stack_a(&a, argv + 1); //cause we want to start at ex: "19 23 21 42"
