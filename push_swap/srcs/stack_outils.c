2ceaa3495c27f4a02fcfdf45c0b12a3e1df637f1
fabocas <fsilva-p@student.42luxembourg.lu>
2024-11-20 18:14:49 +0100

    added

--- push_swap/srcs/stack_outils.c
+++ push_swap/srcs/stack_outils.c
@@ -12,78 +12,76 @@
 
 #include "../include/push_swap.h"
 
-t_stack_nodes *find_lastnode(t_stack_nodes *stack)
+t_stack_nodes	*find_lastnode(t_stack_nodes	*stack)
 {
-    if (!stack)
-        return (NULL);
-    while (stack->next)
-        stack = stack->next;
-    return (stack);
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
 }
 
-bool is_sorted(t_stack_nodes *stack)
+bool	is_sorted(t_stack_nodes	*stack)
 {
-    while (stack && stack->next)
-    {
-        if (stack->nbr > stack->next->nbr)
-            return  (false);
-        stack = stack->next;
-    }
-    return (true);
+	while (stack && stack->next)
+	{
+		if (stack->nbr > stack->next->nbr)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
 }
 
-int stack_length(t_stack_nodes *stack)
+int	stack_length(t_stack_nodes	*stack)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!stack)
-        return (0);
-    while (stack)
-    {
-        i++;
-        stack = stack->next;
-    }
-    return i;
+	i = 0;
+	if (!stack)
+		return (0);
+	while (stack)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
 }
 
-t_stack_nodes *find_maxnode(t_stack_nodes *stack)
+t_stack_nodes	*find_maxnode(t_stack_nodes	*stack)
 {
-    long    max;  //max stack number
-    t_stack_nodes  *max_node; //pointer that points to the biggest number
+	long	max; //max stack number
+	t_stack_nodes	*max_node; //pointer that points to the biggest number
 
-    max = LONG_MIN;  //long min so we dont need extra conditions or check for negative and positive numbers
-    if (!stack)
-        return (NULL);
-    while (stack)
-    {
-        if (stack->nbr > max)     //nbr > long_min  imagine nbr is -2 > long_min so -2 is the max so far
-        {
-            max = stack->nbr;   //ex:max = -2
-            max_node = stack;  //biggest number = node
-        }
-        stack = stack->next;     // move to the next node
-    }
-    return max_node; //returns pointer to the biggest number
+	max = LONG_MIN;  //long min so we dont need extra conditions or check for negative and positive numbers
+	if (!stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->nbr > max)     //nbr > long_min  imagine nbr is -2 > long_min so -2 is the max so far
+		{
+			max = stack->nbr;   //ex:max = -2
+			max_node = stack;  //biggest number = node
+		}
+		stack = stack->next;     // move to the next node
+	}
+    return (max_node); //returns pointer to the biggest number
 }
 
-t_stack_nodes *find_minnode(t_stack_nodes *stack)
+t_stack_nodes	*find_minnode(t_stack_nodes	*stack)
 {
-    long    min;  //min stack number
-    t_stack_nodes  *min_node; //pointer that points to the smallest number
-
-    
-    if (!stack)
-        return (NULL);
-    min = LONG_MAX;
-    while (stack)
-    {
-        if (stack->nbr < min)
-        {     
-            min = stack->nbr;
-            min_node = stack;
-        }
-        stack = stack->next;     // move to the next node
-    }
-    return min_node; //returns pointer to the smallest number
+	long			min;  //min stack number
+	t_stack_nodes	*min_node; //pointer that points to the smallest number
+	if (!stack)
+		return (NULL);
+	min = LONG_MAX;
+	while (stack)
+	{
+		if (stack->nbr < min)
+		{
+			min = stack->nbr;
+			min_node = stack;
+		}
+		stack = stack->next; // move to the next node
+	}
+	return (min_node); //returns pointer to the smallest number
 }
