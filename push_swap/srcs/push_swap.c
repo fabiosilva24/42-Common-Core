1f8953f47169d151a4337714ad9671fb61fb54a2
fabocas <fabio_bastos03@hotmail.com>
2024-11-24 20:51:49 +0100

    added

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -12,29 +12,28 @@
 
 #include "../include/push_swap.h"
 
-int main(int    argc, char  **argv)
+int	main(int argc, char **argv)
 {
-    t_stack_nodes   *a;
-    t_stack_nodes   *b;
+	t_stack_nodes	*a;
+	t_stack_nodes	*b;
 
-    a = NULL;
-    b = NULL;
-    
-	if (argc == 1  || (argc == 2 && !argv[1][0]))
-        return (1);
-    else if (argc == 2)
-        argv = modified_ftsplit(argv[1], ' ');
-    init_stack_a(&a, argv + 1); //cause we want to start at ex: "19 23 21 42"
-    if (!is_sorted(a))
-    {
-        if (stack_length(a) == 2)
-            sa(&a);
-        else if (stack_length(a) == 3)
-            sort_three(&a);
-        else
-            sort_stacks(&a, &b);
-    }
-    free_stack(&a);
-    free_argv(argv);
-    return (0);
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = modified_ftsplit(argv[1], ' ');
+	init_stack_a(&a, argv + 1); //cause we want to start at ex: "19 23 21 42"
+	if (!is_sorted(a))
+	{
+		if (stack_length(a) == 2)
+			sa(&a);
+		else if (stack_length(a) == 3)
+			sort_three(&a);
+		else
+			sort_stacks(&a, &b);
+	}
+	free_stack(&a);
+	free_argv(argv);
+	return (0);
 }
