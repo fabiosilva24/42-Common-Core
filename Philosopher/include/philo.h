54b2dbe36ed95ee743fa724a7ee5ca8382ca1d31
Fabio Andre Silva Pinto Bastos <fsilva-p@c1r8s3.42luxembourg.lu>
2024-12-04 21:07:59 +0100

    added

--- Philosopher/include/philo.h
+++ Philosopher/include/philo.h
@@ -1,5 +1,45 @@
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 
+
+
+typedef struct s_simulation
+{
+	int					num_philosophers;
+	int					time_to_die;
+	int					time_to_sleep;
+	int					time_to_eat;
+	int					max_meals;
+	int					end_simulation;
+
+	pthread_mutex_t 	*forks;
+	pthread_mutex_t		print_mutex;
+	pthread_mutex_t 	death_mutex;
+
+	long long			start_time;
+} t_simulation;
+
+typedef struct s_philo
+{
+	int					id;
+	int					meals_eaten;
+	int					left_forks_id;
+	int					right_forks_id;
+
+	pthread_t			thread;
+	t_simulation		*simulation;
+	long long			last_meal_time;
+}
+
+int		init_simulation(t_simulation *sim, int argc, char **argv);
+void 	*philo_routine(void *arg);
+
+
+
+#endif
---
+++ Philosopher/srcs/philo_routine.c
@@ -0,0 +1,7 @@
+#include "./include/philo.h"
+
+void 	*philo_routine(void *arg)
+{
+	
+
+}
