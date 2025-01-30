7bd0121a7eb616fb8f6276947173c548b1b7bfe0
fabio <155982713+fabiosilva24@users.noreply.github.com>
2025-01-30 09:45:24 +0100

    Atualizar o my_echo.c

--- minishell/srcs/my_echo.c
+++ minishell/srcs/my_echo.c
@@ -1,11 +1,18 @@
 void my_echo(int argc, char **argv)
 {
   int i;
+  int newline;
   
   
   i = 1;
+  newline = 1
   
-  while (argc > 1 && strcmp(argv[i]) == 0)
+  if (argc > 1 && strcmp(argv[i]) == 0)
+  {
+     newline == 0
+     i = 1
+  }
+  i++
 
 }
   
