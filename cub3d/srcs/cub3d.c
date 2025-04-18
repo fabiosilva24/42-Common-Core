bfe4c3f9d60076b9f377e41d5bffd3f31751adab
Fabio Andre Silva Pinto Bastos <fsilva-p@c2r2s1.42luxembourg.lu>
2025-04-18 18:19:21 +0200

    added

--- cub3d/srcs/cub3d.c
+++ cub3d/srcs/cub3d.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
     game.mlx_ptr = mlx_init();
-    game.win_ptr = mlx_new_window(game.mlx_ptr, 800, 800, "CUB3D");
+    game.win_ptr = mlx_new_window(game.mlx_ptr, 1920, 1080, "CUB3D");
     mlx_loop(game.mlx_ptr);
     return (0);
 }
